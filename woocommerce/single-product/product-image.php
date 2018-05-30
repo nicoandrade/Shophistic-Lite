fd264937a1e72440eae8923b7e6db9f1d210759d
Nico Andrade <nicoandrade@gmail.com>
2018-05-30 14:26:43 -0300

    1.0.19
    - Fixed: Text domain

--- woocommerce/single-product/product-image.php
+++ woocommerce/single-product/product-image.php
@@ -84,7 +84,7 @@ $wrapper_classes   = apply_filters( 'woocommerce_single_product_image_gallery_cl
 					} else {
 
 						$html  = '<div class="woocommerce-product-gallery__image--placeholder">';
-						$html .= sprintf( '<img src="%s" alt="%s" class="wp-post-image" />', esc_url( wc_placeholder_img_src() ), esc_html__( 'Awaiting product image', 'woocommerce' ) );
+						$html .= sprintf( '<img src="%s" alt="%s" class="wp-post-image" />', esc_url( wc_placeholder_img_src() ), esc_html__( 'Awaiting product image', 'shophistic-lite' ) );
 						$html .= '</div>';
 
 						echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', $html, $post_thumbnail_id );
