253330a9ab5d017e74e8823dc71be2da54c8a08d
lenivene <lenivene@users.noreply.github.com>
2018-12-06 22:55:50 -0200

    Update post_image.php
    No required "echo" to execute and "esc_url" function.
    
    'the_permalink' already has as functions

--- post_image.php
+++ post_image.php
@@ -5,7 +5,7 @@ if ( has_post_thumbnail() ) {
 	<?php 
 	if ( ! is_single() ) {
 	?>
-	<a href="<?php echo esc_url( get_permalink() ) ?>" class="ql_thumbnail_hover" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
+	<a href="<?php the_permalink(); ?>" class="ql_thumbnail_hover" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
 		<span><i class="fa fa-chevron-right"></i></span>
 	<?php } ?>
 
