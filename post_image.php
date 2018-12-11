90350a967390a2c9832b6a602365b9d935a28b57
Nico Andrade <nicoandrade@gmail.com>
2018-12-11 12:13:46 -0300

    1.0.20
    - Fixed: Search products button
    - Fixed: Slidemenu bug (@dontbyte)
    - Added: WooCommerce 3.5.2 support
    - Improved: Permalink for post image (@lenivene)

--- post_image.php
+++ post_image.php
@@ -1,16 +1,16 @@
 <?php
-if ( has_post_thumbnail() ) {
-?>
+    if ( has_post_thumbnail() ) {
+    ?>
 <div class="post-image">
-	<?php 
-	if ( ! is_single() ) {
-	?>
-	<a href="<?php echo esc_url( get_permalink() ) ?>" class="ql_thumbnail_hover" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
+	<?php
+        if ( ! is_single() ) {
+            ?>
+	<a href="<?php the_permalink() ?>" class="ql_thumbnail_hover" rel="bookmark" title="Permanent Link to<?php the_title_attribute(); ?>">
 		<span><i class="fa fa-chevron-right"></i></span>
 	<?php } ?>
 
-		<?php the_post_thumbnail('post'); ?>
-	
+		<?php the_post_thumbnail( 'post' ); ?>
+
 	<?php if ( ! is_single() ) { ?>
 	</a>
 	<?php } ?>
